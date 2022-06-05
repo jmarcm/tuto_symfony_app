a48288c6c660a2d7d0cd658c41164da54bf74827
Jean-Marc <mjean_marc@hotmail.com>
2022-06-05 11:47:50 +0200

    Utilisation des objets Request et Response

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,15 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class HelloController extends AbstractController {
 
-    function hello() {
+    function hello(Request $request) {
 
-        $title = 'Utilisateurs';
-        $users = ['Aurélie', 'Léa', 'Pierrick', 'Pierre'];
+        // $title = 'Utilisateurs';
+        // $users = ['Aurélie', 'Léa', 'Pierrick', 'Pierre'];
 
-        return $this->render('hello.html.twig', ['title' => $title, 'users' => $users]);
+        // return $this->render('hello.html.twig', ['title' => $title, 'users' => $users]);
+
+        $params = $request->query->all();
+        $string = 'Les paramètres sont : <br/>';
+        foreach ($params as $key => $value) {
+            $string .= "- " . $key . ":" . $value . "<br/>";
+        }
+        return new Response($string);
+        // echo "<pre>";
+        // var_dump($params);
+        // echo "</pre>";
         
     }
 }
