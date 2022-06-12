242a8a0689ec5c8865b4b13e6cd450746f7432a6
Jean-Marc <mjean_marc@hotmail.com>
2022-06-12 10:44:56 +0200

    Create and update an entity for the DB

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,33 +2,74 @@
 
 namespace App\Entity;
 
-class User {
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
 
-    protected $name;
-    protected $email;
-    protected $date;
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-    function getName() {
+    /**
+     * @ORM\Column(type="string", length=125)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
         return $this->name;
     }
 
-    function setName($name) {
+    public function setName(string $name): self
+    {
         $this->name = $name;
+
+        return $this;
     }
 
-    function getEmail() {
+    public function getEmail(): ?string
+    {
         return $this->email;
     }
 
-    function setEmail($email) {
+    public function setEmail(string $email): self
+    {
         $this->email = $email;
+
+        return $this;
     }
 
-    function getDate() {
+    public function getDate(): ?\DateTimeInterface
+    {
         return $this->date;
     }
 
-    function setDate($date) {
+    public function setDate(?\DateTimeInterface $date): self
+    {
         $this->date = $date;
+
+        return $this;
     }
 }
